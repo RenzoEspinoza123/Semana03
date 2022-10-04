0091e29bc96dc66d4e301c0a06f3aed054f848a5
LENOVO <LENOVO@DESKTOP-6LOFA7C>
2022-10-04 10:27:14 -0500

    Ejercicios - Semana 03

---
+++ src/main/java/com/mycompany/e2709/E2.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.e2709;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class E2 {
+    public static void main(String[] args){
+        
+        int [] [] matriz = new int [10][10];
+        int f=0;
+        int j=0;
+        int suma1 = 0;
+        int suma2 = 0;
+        int b = 1;
+        for (f=0 ; f < matriz.length; f++){
+            for (j=0; j < matriz.length; j++){
+           
+                matriz[f][j]=(int) (Math.random()*100+1);
+            }
+        }
+        for (j=0 ; j < matriz.length; j++){
+            for (f = 0 ; f < matriz.length; f++){
+                if (f+j==matriz.length-1){
+                    suma2 = suma2+matriz[f][j];
+                }
+            }
+        }
+        for (j=0; j < matriz.length ; j++){
+            suma1 = suma1+matriz[j][j];
+        }
+        for (f=0 ; f< matriz.length; f++){
+            System.out.println(Arrays.toString(matriz[f]));
+        }
+        System.out.println("================================================");
+        System.out.println("La suma de la diagonal principal es: " + suma1);
+        System.out.println("La suma de la diagonal secundaria es: " + suma2);
+        System.out.println("El producto de la multiplicacion de la diagonal secundaria y primaria es: " + (suma1*suma2));
+    }
+}
---
+++ src/main/java/com/mycompany/e2709/E4.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.e2709;
+
+import java.util.Arrays;
+
+public class E4 {
+    public static void main(String[] args){
+        
+        int [] [] matriz = new int [10][10];
+        int f=0;
+        int j=0;
+        int suma1 = 0;
+        int suma2 = 0;
+        int b = 1;
+        for (f=0 ; f < matriz.length; f++){
+            for (j=0; j < matriz.length; j++){
+           
+                matriz[f][j]=(int) (Math.random()*100+1);
+            }
+        }
+        for (f=0 ; f < matriz.length; f++){
+            System.out.println(Arrays.toString(matriz[f]));
+        }
+        for (int i=0; i<10; i++){
+                
+            
+            suma2 = 0;
+            for (j=0; j < matriz.length ; j++){
+                suma2 = suma2 + matriz[i][j];
+            }
+
+        System.out.println("La suma de la columna "+i+" es: "+suma2);
+
+    }
+}
+} 
+
